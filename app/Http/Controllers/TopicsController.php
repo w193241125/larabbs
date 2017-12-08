b98a5c319aaa7e6641df8e24f5f56e66d8a251ea
liuguansheng <164722305@qq.com>
2017-12-08 21:10:00 +0800

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,6 +25,10 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -39,8 +43,11 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', '创建成功.');
-	}
+        //return redirect()->route('topics.show', $topic->id)->with('message', '创建成功.');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
+
+
+    }
 
 	public function edit(Topic $topic)
 	{
@@ -54,7 +61,8 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功.');
+		//return redirect()->route('topics.show', $topic->id)->with('message', '更新成功.');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
 	}
 
 	public function destroy(Topic $topic)
